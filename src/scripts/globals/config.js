be032ba38463e0462418f8434c45a7b39072f867
YogaRahmahadi <yogarahmahadi1234@gmail.com>
2023-05-02 20:56:28 +0700

    Melakukan Request dari API

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: "a7b057da3f49873a641b4efd80357e28",
+  BASE_URL: "https://api.themoviedb.org/3/",
+  BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
+  DEFAULT_LANGUAGE: "en-us",
+};
+
+export default CONFIG;
