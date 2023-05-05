40fb8d90c5f989a231c6546f9443fe3dc51eecac
YogaRahmahadi <yogarahmahadi1234@gmail.com>
2023-05-05 10:13:12 +0700

    Menerapkan Cache API pada Service Worker

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,18 +1,33 @@
-self.addEventListener("install", (event) => {
-  console.log("Installing Service Worker ...");
+import "regenerator-runtime";
+import CacheHelper from "./utils/cache-helper";
 
-  // TODO: Caching App Shell Resource
+// Caching the listed asset below
+const assetsToCache = [
+  "./",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
+  "./index.html",
+  "./favicon.png",
+  "./app.bundle.js",
+  "./app.webmanifest",
+  "./sw.bundle.js",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("Activating Service Worker ...");
-
-  // TODO: Delete old caches
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log(event.request);
-
-  event.respondWith(fetch(event.request));
-  // TODO: Add/get fetch request to/from caches
+  if (!(event.request.url.indexOf("http") === 0)) return;
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
